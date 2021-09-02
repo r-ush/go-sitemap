d279dbb0c8cf14683cc5a25232f21284c68abe49
r-ush <aarush.bhatt@gmail.com>
2021-09-02 13:35:38 +0530

    feat: link parser

--- link/parse.go
+++ link/parse.go
@@ -42,7 +42,22 @@ func buildLink(n *html.Node) Link {
 			break
 		}
 	}
-	ret.Text = "TODO:parse"
+	ret.Text = text(n)
+	return ret
+}
+
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c) + " "
+	}
+	ret = strings.Join(strings.Fields(ret), " ")
 	return ret
 }
 
